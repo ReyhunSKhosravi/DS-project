247a9711c5b03f1201b397afe2e0a9216bc666c6
Reyhun <153283408+ReyhunSKhosravi@users.noreply.github.com>
2024-02-07 10:31:51 +0330

    Classes of Bus and Requests

---
+++ ClassBus.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+class Bus
+{
+private:
+    /* data */
+    
+public:
+    static int MoneyCost = 2250; //cost in money per line
+    static int TimeCost = 4; //cost in time per kilometer
+    static int HourTime;
+    Bus(/* args */);
+    ~Bus();
+};
+
+Bus::Bus(/* args */)
+{
+}
+
+Bus::~Bus()
+{
+}
+
+int main(){
+
+    return 0;
+}
