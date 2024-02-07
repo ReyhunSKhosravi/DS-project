bef37d04ec827ba3140cf7f4a7c6a4dcfb45490f
Reyhun <Reyhunskhosravi@gmail.com>
2024-02-07 10:42:44 +0330

    edit .h and .cpp files of classes

---
+++ ClassReq.h
@@ -0,0 +1,23 @@
+//implementation of class of requests
+#include <iostream>
+using namespace std;
+class Request
+{
+private:
+    /* data */
+public:
+    Request(/* args */);
+    ~Request();
+};
+
+Request::Request(/* args */)
+{
+}
+
+Request::~Request()
+{
+}
+
+int main(){
+    return 0;
+}
