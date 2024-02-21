a9f2092d9817ad3486858ea6daf515534af4937f
fateme529 <121169758+fateme529@users.noreply.github.com>
2024-02-21 22:17:32 +0330

    v3.7.3
     find short path with DFS and print

--- src/DFS_Weight.cpp
+++ src/DFS_Weight.cpp
@@ -1,35 +1,46 @@
 #include <iostream>
 #include <list>
 #include <vector>
+#include <map>
 #include <limits>
 
 using namespace std;
 
-// Define graph using adjacency list
 class Graph {
-    int V;              // Number of vertices
+    int V; // Number of vertices
     vector<list<pair<int, int>>> adj; // Adjacency list
+    map<string, int> verticesMap; // Map to store vertices as string to int
 
 public:
-    Graph(int V) : V(V) {
-        adj.resize(V);
-    }
+    Graph() : V(0) {}
 
-    // Add weighted edge to the graph
+    // Function to add a new edge to the graph
     void addEdge(int v, int w, int weight) {
-        adj[v].push_back(make_pair(w, weight)); // Add w to v's list with weight
+        adj[v].push_back(make_pair(w, weight));
     }
 
-    // Helper function for DFS to find the shortest path between two vertices
-    void DFSUtil(int v, int dest, vector<bool>& visited, vector<int>& path, vector<int>& shortestPath, int& shortestPathWeight) {
-        visited[v] = true; // Mark current vertex as visited
+    // Function to add a new vertex to the graph
+    void addVertex(string vertex) {
+        if (verticesMap.find(vertex) == verticesMap.end()) {
+            verticesMap[vertex] = V++;
+            adj.resize(V);
+        }
+    }
 
-        // Add current vertex to path
+    // Function to get the vertex number corresponding to the vertex name
+    int getVertexNumber(string vertex) {
+        if (verticesMap.find(vertex) != verticesMap.end()) {
+            return verticesMap[vertex];
+        }
+        return -1;
+    }
+
+    // Depth-first search (DFS) algorithm to find the shortest path
+    void DFSUtil(int v, int dest, vector<bool>& visited, vector<int>& path, vector<int>& shortestPath, int& shortestPathWeight) {
+        visited[v] = true;
         path.push_back(v);
 
-        // If destination vertex is reached
         if (v == dest) {
-            // Calculate the weight of the current path
             int pathWeight = 0;
             for (int i = 0; i < path.size() - 1; ++i) {
                 for (auto it = adj[path[i]].begin(); it != adj[path[i]].end(); ++it) {
@@ -39,59 +50,76 @@ public:
                     }
                 }
             }
-
-            // If the weight of the current path is less than the weight of the shortest path found so far
             if (pathWeight < shortestPathWeight) {
                 shortestPathWeight = pathWeight;
                 shortestPath = path;
             }
         } else {
-            // If destination vertex is not reached, recursively traverse adjacent vertices and call DFS
-            for (auto i = adj[v].begin(); i != adj[v].end(); ++i)
-                if (!visited[i->first])
+            for (auto i = adj[v].begin(); i != adj[v].end(); ++i) {
+                if (!visited[i->first]) {
                     DFSUtil(i->first, dest, visited, path, shortestPath, shortestPathWeight);
+                }
+            }
         }
 
-        // Remove current vertex from path and mark it as unvisited
         path.pop_back();
         visited[v] = false;
     }
 
-    // Find the shortest path between two vertices
-    vector<int> findShortestPath(int src, int dest) {
-        // Mark all vertices as unvisited
+    // Function to find the shortest path between two vertices
+    vector<int> findShortestPath(string srcStr, string destStr) {
+        int src = verticesMap[srcStr];
+        int dest = verticesMap[destStr];
+
         vector<bool> visited(V, false);
-
-        // For storing the shortest path
         vector<int> path, shortestPath;
-
-        // Initialize shortest path weight to maximum value
         int shortestPathWeight = numeric_limits<int>::max();
 
-        // Use DFS to find the shortest path
         DFSUtil(src, dest, visited, path, shortestPath, shortestPathWeight);
 
         return shortestPath;
     }
-};
-int main() {
-    // Define the graph
-    Graph g(6);
-    g.addEdge(0, 1, 10);
-    g.addEdge(0, 2, 3);
-    g.addEdge(1, 2, 5);
-    g.addEdge(1, 3, 6);
-    g.addEdge(2, 3, 2);
-    g.addEdge(2, 4, 9);
-    g.addEdge(3, 4, 7);
-    g.addEdge(3, 5, 8);
-    g.addEdge(4, 5, 4);
 
-    int src = 0, dest = 5;
-    cout << "Shortest path between " << src << " and " << dest << ":\n";
-    vector<int> shortestPath = g.findShortestPath(src, dest);
-    for (int i = 0; i < shortestPath.size(); ++i)
-        cout << shortestPath[i] << " ";
+    // Function to get the vertices map
+    map<string, int> getVerticesMap() const {
+        return verticesMap;
+    }
+};
+
+int main() {
+    // Create a graph instance
+    Graph g;
+
+    // Add vertices and edges to the graph
+    g.addVertex("Meydan-e_Hazrat-e_ValiAsr");
+    g.addVertex("Haftom-e_Tir");
+    g.addVertex("Emam_Hossein");
+    g.addVertex("Meydan-e_Shohada");
+    g.addVertex("Shahid_Rezaei");
+    g.addVertex("Haram-e_Hazrat-e_Abdolazim");
+
+    g.addEdge(g.getVertexNumber("Meydan-e_Hazrat-e_ValiAsr"), g.getVertexNumber("Haftom-e_Tir"), 4);
+    g.addEdge(g.getVertexNumber("Haftom-e_Tir"), g.getVertexNumber("Emam_Hossein"), 10);
+    g.addEdge(g.getVertexNumber("Emam_Hossein"), g.getVertexNumber("Meydan-e_Shohada"), 4);
+    g.addEdge(g.getVertexNumber("Meydan-e_Shohada"), g.getVertexNumber("Shahid_Rezaei"), 12);
+    g.addEdge(g.getVertexNumber("Shahid_Rezaei"), g.getVertexNumber("Haram-e_Hazrat-e_Abdolazim"), 20);
+
+    string srcStr = "Meydan-e_Hazrat-e_ValiAsr";
+    string destStr = "Haram-e_Hazrat-e_Abdolazim";
+
+    // Find the shortest path
+    vector<int> shortestPath = g.findShortestPath(srcStr, destStr);
+
+    // Display the shortest path
+    cout << "Shortest path between " << srcStr << " and " << destStr << ":\n";
+    for (int i = 0; i < shortestPath.size(); ++i) {
+        for (auto it = g.getVerticesMap().begin(); it != g.getVerticesMap().end(); ++it) {
+            if (it->second == shortestPath[i]) {
+                cout << it->first << " ";
+                break;
+            }
+        }
+    }
     cout << endl;
 
     return 0;
