4233212c48ddccc6ddc4cebf97bf10e78df7e521
zeslami <122128104+zeslami@users.noreply.github.com>
2024-03-01 23:40:08 +0330

    v2.0.0

---
+++ include/app.hpp
@@ -0,0 +1,23 @@
+#ifndef SMHT_HPP
+#define SMHT_HPP
+#include <queue>
+#include <iostream>
+#include "time.hpp"
+#include "ClassReq.hpp"
+
+using namespace std;
+
+class APP  // samaneh modiriate haml-o naghle tehran.
+{
+public:
+    void start();
+
+private:
+    int n;
+    string hour;
+    string src, dst;
+};
+
+
+
+#endif
