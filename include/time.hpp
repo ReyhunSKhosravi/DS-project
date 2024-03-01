ecefa308384d146844e52246705df8c6d6d78809
zeslami <122128104+zeslami@users.noreply.github.com>
2024-03-01 23:41:02 +0330

    v3.0.0

---
+++ include/time.hpp
@@ -0,0 +1,34 @@
+#ifndef TIME_HPP
+#define TIME_HPP
+
+#include<iostream>
+#include<unordered_map>
+#include<vector>
+#include<limits.h>
+
+using namespace std;
+
+class Time
+{
+    public:
+        Time(){};
+        Time(string time);
+        void set_noon(string noon);
+        void set_hour(int h);
+        void set_minute(int m);
+        void print();
+
+        Time operator+=(int min);
+        friend istream &operator>>(istream &input, Time &time);
+
+        int get_hour();
+        int get_minute();
+        string get_noon();
+
+    private :
+        int hour = 0;
+        int minute = 0;
+        string noon;
+};
+
+#endif
